597009d79f58416dabe53eb7b34ebe3ce9c35455
Waflon <alvaro1542@gmail.com>
2024-07-16 12:12:14 -0400

    agregado modelo Data

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Data(BaseModel):
+    name:str
+
+@app.get("/crear/")
+async def crear(data: Data):
+    return {data}
+
 
 @app.get("/test/{item_id}")
 async def test(item_id: str):
